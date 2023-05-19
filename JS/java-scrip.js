4b2f19202d072eab373720fd8ac092d5ac6144d1
7ShIkI3 <78133376+7ShIkI3@users.noreply.github.com>
2023-05-19 21:31:38 +0200

    Big "js" update and correction with chatgpt
    sorry i use chatgpt for correct my code

--- JS/java-scrip.js
+++ JS/java-scrip.js
@@ -1,18 +1,13 @@
-
 function init() {
-  // on efface l'élément de la page html dont l'ID est 'listReverse'
   document.getElementById('genMDP').value = "";
   document.getElementById('verifyMDP').value = "";
-  }
+}
 
-  function getList() {
-    let tab = document.getElementById('genMDP').value;
-    let result = [];
-    for (let c = 0; c < tab.length; c++) {
-      result.push(tab[c]);
-    }
-    return result;
-  }
+function getList() {
+  let tab = document.getElementById('genMDP').value;
+  let list = tab.split('|');
+  return list.map(item => item.trim());
+}
 
 function generatePassword() {
   const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&;|_~&%§*"-`+=]()).{8,}$/;
@@ -35,23 +30,18 @@ function verifyPassword() {
   const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&;|_~&%§*"-`+=]()).{8,}$/;
   let list = getList();
   
-  if (list && list.firstChild) {
-    let check = list.firstChild;
-    const checkedValue = check.textContent.match(regex);
+  if (list.length > 0) {
+    let check = list[0];
+    const checkedValue = check.match(regex); // Use match on the check string
 
     if (checkedValue === null) {
-      check.insertAdjacentHTML("afterbegin", "<li>Invalid Password!</li>");
+      check = "Invalid Password!"; // Add the error message before the check value
     } else {
-      //check.insertAdjacentHTML("afterbegin", "<li>" + checkedValue[0] + "</li>"); // Commented out since checkedValue.group() is not applicable in JavaScript
-      check.insertAdjacentHTML("afterbegin", "<li>Good Password</li>");
+      check = "Good Password"; // Add the success message before the check value
     }
-    document.getElementById('verifyMDP').value = check.textContent;
-    console.log(check.textContent);
+    document.getElementById('verifyMDP').value = check;
+    console.log(check);
   } else {
-    console.log("Error: No list or list.firstChild found.");
+    console.log("Error: No list element found.");
   }
 }
-
-
-
-
