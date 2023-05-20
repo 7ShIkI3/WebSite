8323e79e5e9d9c47443d9500261e6c7fba5c2127
7ShIkI3 <78133376+7ShIkI3@users.noreply.github.com>
2023-05-20 10:19:42 +0200

    Insane update by @idootop
    @idootop change the HTML and CSS and the JS
    
    Co-Authored-By: Del <35302658+idootop@users.noreply.github.com>

--- JS/java-scrip.js
+++ JS/java-scrip.js
@@ -1,35 +1,52 @@
+function showMenu() {
+  const elements = Array.from(document.getElementsByClassName("menu-panel"));
+  elements.forEach((e) => {
+    e.style.visibility = "visible";
+  });
+}
+
+function hideMenu() {
+  const elements = Array.from(document.getElementsByClassName("menu-panel"));
+  elements.forEach((e) => {
+    e.style.visibility = "hidden";
+  });
+}
+
 function init() {
-  document.getElementById('genMDP').value = "";
-  document.getElementById('verifyMDP').value = "";
+  document.getElementById("genMDP").value = "";
+  document.getElementById("verifyMDP").value = "";
 }
 
 function getList() {
-  let tab = document.getElementById('genMDP').value;
-  let list = tab.split('|');
-  return list.map(item => item.trim());
+  let tab = document.getElementById("genMDP").value;
+  let list = tab.split("|");
+  return list.map((item) => item.trim());
 }
 
 function generatePassword() {
-  const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&;|_~&%§*"-`+=]()).{8,}$/;
-  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#?!@$%^&;|_~&%§*"-`+=';
+  const regex =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&;|_~&%§*"-`+=]()).{8,}$/;
+  const characters =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#?!@$%^&;|_~&%§*"-`+=';
 
-  let password = '';
+  let password = "";
   while (!regex.test(password)) {
-    password = '';
+    password = "";
     const randomLength = Math.floor(Math.random() * 8) + 8; // Random length between 8 and 15
     for (let i = 0; i < randomLength; i++) {
       const randomIndex = Math.floor(Math.random() * characters.length);
       password += characters[randomIndex];
     }
   }
-  document.getElementById('genMDP').value = password;
+  document.getElementById("genMDP").value = password;
   console.log(password);
 }
 
 function verifyPassword() {
-  const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&;|_~&%§*"-`+=]()).{8,}$/;
+  const regex =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&;|_~&%§*"-`+=]()).{8,}$/;
   let list = getList();
-  
+
   if (list.length > 0) {
     let check = list[0];
     const checkedValue = check.match(regex); // Use match on the check string
@@ -39,7 +56,7 @@ function verifyPassword() {
     } else {
       check = "Good Password"; // Add the success message before the check value
     }
-    document.getElementById('verifyMDP').value = check;
+    document.getElementById("verifyMDP").value = check;
     console.log(check);
   } else {
     console.log("Error: No list element found.");
