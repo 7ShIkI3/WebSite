8a8281260052f226f0f485caa034ebb3491254f2
7ShIkI3 <78133376+7ShIkI3@users.noreply.github.com>
2023-06-14 16:14:25 +0200

    test updatede

--- JS/java-scrip.js
+++ JS/java-scrip.js
@@ -40,47 +40,26 @@ function generatePassword() {
   document.getElementById("genMDP").value = password;
   console.log(password);
 }
-const crypto = require('crypto');
-
-// Fonction pour hacher le mot de passe
-function hashPassword(password) {
-  const hash = crypto.createHash('sha256');
-  hash.update(password);
-  return hash.digest('hex');
-}
-
-// Fonction pour vérifier si le mot de passe est compromis
-function isPasswordCompromised(password, database) {
-  const hashedPassword = hashPassword(password);
-
-  // Vérifier si le mot de passe haché se trouve dans la base de données
-  return database.includes(hashedPassword);
-}
-
-// Exemple d'utilisation
-const database = ['b5f7e53d0a87a7ef0a49dd4a60e8fffa']; // Exemple d'un mot de passe haché enregistré dans la base de données
-
-const password = 'monmotdepasse';
-const isCompromised = isPasswordCompromised(password, database);
-
 
 function verifyPassword() {
-  const password = document.getElementById("verifyMDP").value;
+  const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&;|_~&%§*"-`+=]()).{8,}$/;
   let list = getList();
 
   if (list.length > 0) {
-    const isCompromised = isPasswordCompromised(password, list);
+    let check = list[0];
+    const checkedValue = check.match(regex); // Utiliser match sur la chaîne check
 
-    if (isCompromised) {
-      document.getElementById("verifyMDP").value = "Invalid Password!";
+    if (checkedValue === null) {
+      check = "Invalid Password!"; // Ajouter le message d'erreur avant la valeur check
       document.getElementById("verifyMDP").classList.add('invalid');
       document.getElementById("verifyMDP").classList.remove('valid');
     } else {
-      document.getElementById("verifyMDP").value = "Good Password!";
+      check = "Good Password!"; // Ajouter le message de réussite avant la valeur check
       document.getElementById("verifyMDP").classList.add('valid');
       document.getElementById("verifyMDP").classList.remove('invalid');
     }
-    console.log(document.getElementById("verifyMDP").value);
+    document.getElementById("verifyMDP").value = check;
+    console.log(check);
   } else {
     console.log("Error: No list element found.");
   }
